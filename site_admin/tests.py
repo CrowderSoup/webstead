7189306e4b05981df05974d01fd4d1e55731a6ed
Aaron Crowder <aaron.crowder@gmail.com>
2026-01-11 20:03:59 -0700

    Fixes dark-mode bug

--- site_admin/tests.py
+++ site_admin/tests.py
@@ -57,6 +57,15 @@ class SiteAdminAccessTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    def test_admin_bar_hides_theme_toggle_on_site(self):
+        self.client.force_login(self.staff)
+        response = self.client.get(reverse("site_admin:admin_bar"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "data-admin-theme-toggle")
+        self.assertNotContains(response, "Dark mode")
+        self.assertNotContains(response, "Light mode")
+
 
 class SiteAdminPageTests(TestCase):
     def setUp(self):
